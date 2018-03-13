cf6dc2b874f7fd3c269e16deef0b2369ef2194d4
Alejandro Martin <contact@alejandromartin.dev>
2018-03-12 22:44:24 -0600

    First commit

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SOURCE_DIR=src
+TARGET_DIR=docs
+STYLE_SHEET=foundation.css
+STYLES="-a stylesdir=stylesheets/ -a stylesheet=$STYLE_SHEET"
+#STYLES=""
+
+asciidoctor $STYLES $SOURCE_DIR/01-debian-minimal-installation.adoc -o $TARGET_DIR/index.html
+asciidoctor $STYLES $SOURCE_DIR/02-debian-post-installation.adoc -o $TARGET_DIR/index-02.html
+asciidoctor $STYLES $SOURCE_DIR/03-debian-developer.adoc -o $TARGET_DIR/index-03.html
+asciidoctor $STYLES $SOURCE_DIR/04-debian-gnome-configuration.adoc -o $TARGET_DIR/index-04.html
