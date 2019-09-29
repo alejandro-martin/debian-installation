9d642a3d013969e9dda0e6b2548fd72b473e816f
Alejandro Martin <contact@alejandromartin.dev>
2019-09-28 19:09:10 -0500

    Added Debian 10 Guide.

--- build.sh
+++ build.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
 
-SOURCE_DIR=src
+# initial config
+
+INCLUDE_DIR=src/include
+TARGET_DIR=docs
+
+rm -r $TARGET_DIR/*
+mkdir $TARGET_DIR/img
+cp $INCLUDE_DIR/index.html $TARGET_DIR/
+
+# debian 9 - Stretch
+
+INCLUDE_DIR=include
+SOURCE_DIR=src/debian-9
 TARGET_DIR=docs
 STYLE_SHEET=foundation.css
 STYLES="-a stylesdir=stylesheets/ -a stylesheet=$STYLE_SHEET"
 #STYLES=""
 
-asciidoctor $STYLES $SOURCE_DIR/01-debian-minimal-installation.adoc -o $TARGET_DIR/index.html
-asciidoctor $STYLES $SOURCE_DIR/02-debian-post-installation.adoc -o $TARGET_DIR/index-02.html
-asciidoctor $STYLES $SOURCE_DIR/03-debian-developer.adoc -o $TARGET_DIR/index-03.html
-asciidoctor $STYLES $SOURCE_DIR/04-debian-gnome-configuration.adoc -o $TARGET_DIR/index-04.html
+cp $SOURCE_DIR/$INCLUDE_DIR/img/* $TARGET_DIR/img
+
+asciidoctor $STYLES $SOURCE_DIR/01-debian-minimal-installation.adoc -o $TARGET_DIR/debian-9-01.html
+asciidoctor $STYLES $SOURCE_DIR/02-debian-post-installation.adoc -o $TARGET_DIR/debian-9-02.html
+asciidoctor $STYLES $SOURCE_DIR/03-debian-developer.adoc -o $TARGET_DIR/debian-9-03.html
+asciidoctor $STYLES $SOURCE_DIR/04-debian-gnome-configuration.adoc -o $TARGET_DIR/debian-9-04.html
+
+# debian 10 - Buster
+
+INCLUDE_DIR=include
+SOURCE_DIR=src/debian-10
+TARGET_DIR=docs
+STYLE_SHEET=foundation.css
+# STYLES="-a stylesdir=stylesheets/ -a stylesheet=$STYLE_SHEET"
+STYLES=""
+
+cp $SOURCE_DIR/$INCLUDE_DIR/img/* $TARGET_DIR/img
+
+asciidoctor $STYLES $SOURCE_DIR/01-debian-minimal-installation.adoc -o $TARGET_DIR/debian-10-01.html
+asciidoctor $STYLES $SOURCE_DIR/02-debian-post-installation.adoc -o $TARGET_DIR/debian-10-02.html
+asciidoctor $STYLES $SOURCE_DIR/03-debian-developer.adoc -o $TARGET_DIR/debian-10-03.html
+asciidoctor $STYLES $SOURCE_DIR/04-debian-gnome-configuration.adoc -o $TARGET_DIR/debian-10-04.html
---
+++ src/debian-9/build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# debian 10 - Buster
+# script used for local build
+
+SOURCE_DIR=
+TARGET_DIR=docs
+INCLUDE_DIR=include
+STYLE_SHEET=foundation.css
+STYLES="-a stylesdir=stylesheets/ -a stylesheet=$STYLE_SHEET"
+#STYLES=""
+
+rm -r $TARGET_DIR/*
+mkdir $TARGET_DIR/img
+cp $INCLUDE_DIR/img/* $TARGET_DIR/img
+asciidoctor 01-debian-minimal-installation.adoc -o $TARGET_DIR/debian-9-01.html
+asciidoctor 02-debian-post-installation.adoc -o $TARGET_DIR/debian-9-02.html
+asciidoctor 03-debian-developer.adoc -o $TARGET_DIR/debian-9-03.html
+asciidoctor 04-debian-gnome-configuration.adoc -o $TARGET_DIR/debian-9-04.html
